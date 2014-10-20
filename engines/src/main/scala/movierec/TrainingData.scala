83a75ab31c738ae0a2861ff395fb273d3b5fd487
Mimi <nqnliu@hotmail.com>
2014-10-19 22:05:45 -0700

    Added Movie and User to TrainingData.scala

--- engines/src/main/scala/movierec/TrainingData.scala
+++ engines/src/main/scala/movierec/TrainingData.scala
@@ -9,3 +9,20 @@ class Rating(
 ) extends Serializable {
   override def toString = "(" + userId + "," + movieId + ")"
 }
+
+class Movie(
+  val movieId: Int
+  val movieTitle: Int
+  val releaseDate: Date
+  val videoReleaseDate: Date
+  val imdbUrl: String
+  // TODO: decide how to implement genre flags
+) extends Serializable
+
+class User(
+  val userId: Int
+  val age: Int
+  val gender: Char
+  val occupation: String
+  val zipCode: Int
+) extends Serializable
